3b1ffadaffeb251e808e4e630dcd51b56a3f063d
devlitus <developercarles@gmail.com>
2018-09-10 08:41:33 +0200

    sincro

--- src/pages/peliculas/peliculas.ts
+++ src/pages/peliculas/peliculas.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, InfiniteScroll } from 'ionic-angular';
 import { MoviProvider } from "../../providers/movi/movi";
 import { Configuracion } from "../../app/configuracion";
 
@@ -10,6 +10,7 @@ import { Configuracion } from "../../app/configuracion";
   templateUrl: 'peliculas.html',
 })
 export class PeliculasPage {
+  @ViewChild(InfiniteScroll) infiniteScroll: InfiniteScroll;
   public configuracion: Configuracion = new Configuracion;
   public pelicula: Object;
   public pagina: number = 0;
@@ -17,14 +18,18 @@ export class PeliculasPage {
   }
 
   ionViewDidLoad() {
-    
-    
+
+
   }
-  getPeliculas(){
-    let pagina = this.pagina+1;
-    this._servicePeliculas.peliculas(pagina).then(response => {
-      this.pelicula = response;
-    })
+  getPeliculas() {
+    let pagina = this.pagina + 1;
+    return this.infiniteScroll
+      .waitFor(this._servicePeliculas
+        .peliculas(pagina)
+        .then(response => {
+          this.pelicula = response;
+          this.infiniteScroll.complete();
+        }))
   }
 
 }
